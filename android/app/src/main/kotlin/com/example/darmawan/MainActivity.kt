6d42774a6130cc54ecb310433d2a076aaf8ebd93
I Ketut Darmawan Wirakusuma <darmawanwirakusuma@gmail.com>
2026-09-15 11:01:30 +0800

    Simpan proyek awal perkuliahan

---
+++ android/app/src/main/kotlin/com/example/darmawan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.darmawan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/namadomain/darmawan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.namadomain.darmawan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
